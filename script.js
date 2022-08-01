6944c28e15cf2cd696dfb49c2a287a3c1cc53c49
Matthew Egan <69609987+mattegan111@users.noreply.github.com>
2022-08-01 19:18:12 +1200

    Place mines into a gameboard solution

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 let targetRowColCoords = []
 
 window.onclick = (e) => {
-  targetRowColCoords = [e.target.className, e.target.parentElement.className]
+  targetRowColCoords = [e.target.className.split(" ")[0], e.target.parentElement.className]
+  console.log(targetRowColCoords)
 }
+
+const gameboardSolution = [
+  ['☐', '☐', '☐', '☐', '☐'],
+  ['☐', '☐', '☐', '☐', '☐'],
+  ['☐', '☐', '☐', '☐', '☐'],
+  ['☐', '☐', '☐', '☐', '☐'],
+  ['☐', '☐', '☐', '☐', '☐']
+]
+
+function placeMines() {
+  gameboardSolution.forEach(column => {
+    column.forEach(cell => {
+      if (Math.random() > 0.5){
+        cell.innerHTML = x
+      }
+    })
+  })
+}
+
+placeMines()
