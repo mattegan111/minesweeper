9e02aba727954d6accdbd6b7f9db21fe7fbdb585
Matthew Egan <69609987+mattegan111@users.noreply.github.com>
2022-08-01 19:48:56 +1200

    Handle left and right click DOM manipulation

--- script.js
+++ script.js
@@ -2,7 +2,16 @@ let targetRowColCoords = []
 
 window.onclick = (e) => {
   targetRowColCoords = [e.target.className.split(" ")[0], e.target.parentElement.className]
-  console.log(targetRowColCoords)
+  e.target.innerHTML = gameboardSolution[targetRowColCoords[0]][targetRowColCoords[1]]
+}
+
+window.oncontextmenu = (e) => {
+  e.preventDefault()
+  if(e.target.innerHTML == '☐'){
+    e.target.innerHTML = '?'
+  } else if(e.target.innerHTML == '?'){
+    e.target.innerHTML = '☐'
+  }
 }
 
 const gameboardSolution = [
@@ -18,6 +27,8 @@ function placeMines() {
     column.forEach((row, rowKey) => {
       if (Math.random() > 0.5){
         gameboardSolution[colKey][rowKey] = 'x'
+      } else {
+        gameboardSolution[colKey][rowKey] = 'o'
       }
     })
   })
