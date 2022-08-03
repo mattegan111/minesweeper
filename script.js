37a0f5ea26da7b96ec8b52e1455ab6d779598247
Matthew Egan <mnegan93@gmail.com>
2022-08-03 23:40:43 +1200

    Generate cells with row and column values

--- script.js
+++ script.js
@@ -7,3 +7,23 @@ class Cell {
     this.minesNearbyCount = minesNearbyCount;
   }
 }
+
+const sideLength = 10
+const cells = []
+
+for (let i = 0; i < sideLength; i++) {
+  let row = i
+  for (let j = 0; j < sideLength; j++) {
+    let column = j
+    let cell = new Cell(
+      false, 
+      false, 
+      row, 
+      column, 
+      0, 
+    )
+    cells.push(cell)
+  }
+}
+
+console.log(cells)
